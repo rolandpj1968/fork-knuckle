887e031f446b75fb05a7e858d148ba0b55a9d22a
rolandpj <rolandpj@gmail.com>
2017-12-05 18:15:25 +0100

    copied from perft.c

---
+++ fork-knuckle.hpp
@@ -0,0 +1,41 @@
+#ifndef PERFTCPP_HPP
+#define PERFTCPP_HPP
+
+/* capture codes */
+static const int C_ORTH =    1;
+static const int C_DIAG =    2;
+static const int C_KNIGHT =  4;
+static const int C_SIDE =    8;
+static const int C_FORW =    0x10;
+static const int C_FDIAG =   0x20;
+static const int C_BACKW =   0x40;
+static const int C_BDIAG =   0x80;
+static const int C_FERZ =    (C_FDIAG|C_BDIAG);
+static const int C_WAZIR =   (C_SIDE|C_FORW|C_BACKW);
+static const int C_KING =    (C_FERZ|C_WAZIR);
+static const int C_BISHOP =  (C_FERZ|C_DIAG);
+static const int C_ROOK =    (C_WAZIR|C_ORTH);
+static const int C_PPAWN =   (C_FDIAG);
+static const int C_MPAWN =   (C_BDIAG);
+static const int C_QUEEN =   (C_BISHOP|C_ROOK);
+static const int C_CONTACT = (C_KING|C_KNIGHT);
+static const int C_DISTANT = (C_ORTH|C_DIAG);
+
+/* board codes (32 per side: each 16 Pieces, 16 Pawns) */
+static const int WHITE =  0x20;
+static const int BLACK =  0x40;
+static const int COLOR =  (BLACK|WHITE);
+static const int GUARD =  (COLOR|0x80);
+static const int DUMMY =  (WHITE-1+0x80);
+static const int PAWNS =  0x10;
+
+static const int NPCE =    (2*WHITE);
+
+static const int KING =   7;
+static const int QUEEN =  6;
+static const int ROOK =   5;
+static const int BISHOP = 4;
+static const int KNIGHT = 3;
+
+
+#endif //def PERFTCPP_HPP
